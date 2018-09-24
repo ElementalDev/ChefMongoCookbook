a0956f505c7f19618eb920600a1e3259a421370e
Reece_Collett <rcollett@spartaglobal.com>
2018-09-24 14:55:15 +0100

    Created mongo cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,28 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_repository "mongodb-org" do
+  keyserver "hkp://keyserver.ubuntu.com:80"
+  key "EA312927"
+  uri "http://repo.mongodb.org/apt/ubuntu"
+  distribution "xenial/mongodb-org/3.2"
+  components ["multiverse"]
+  action :add
+end
+
+apt_update "update" do
+  action :update
+end
+
+package "mongodb-org" do
+  action :install
+end
+
+package "mongodb-org" do
+  action :upgrade
+end
+
+service "mongod" do
+  action [:enable, :start]
+end
